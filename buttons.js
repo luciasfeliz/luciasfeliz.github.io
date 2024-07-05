574f3bce39bd9c2eccc19a9eac1d57a4607b2919
radiak1na <77307627+luciasfeliz@users.noreply.github.com>
2024-07-05 17:01:02 -0400

    Added stuff

---
+++ buttons.js
@@ -0,0 +1,80 @@
+document.getElementById("dni").addEventListener("click", function() {
+    document.getElementById("DNI").style.display = "block";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("reviive").addEventListener("click", function() {
+    document.getElementById("Reviive").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("extended").addEventListener("click", function() {
+    document.getElementById("Extended").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("interests").addEventListener("click", function() {
+    document.getElementById("Interests").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("kins").addEventListener("click", function() {
+    document.getElementById("Kins").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("alters").addEventListener("click", function() {
+    document.getElementById("Alters").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("songs").addEventListener("click", function() {
+    document.getElementById("Songs").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Credits").style.display = "none";
+});
+document.getElementById("credits").addEventListener("click", function() {
+    document.getElementById("Credits").style.display = "block";
+    document.getElementById("DNI").style.display = "none";
+    document.getElementById("Reviive").style.display = "none";
+    document.getElementById("Extended").style.display = "none";
+    document.getElementById("Interests").style.display = "none";
+    document.getElementById("Kins").style.display = "none";
+    document.getElementById("Alters").style.display = "none";
+    document.getElementById("Songs").style.display = "none";
+});
